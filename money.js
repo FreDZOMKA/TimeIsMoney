588d4c5cf30d26de028e212033eaeb0e53acdb65
017-Fredrik <107189080+FreDZOMKA@users.noreply.github.com>
2024-07-23 20:37:06 +0200

    Expansion Time and Salary
    Expanding to select time unit month day year hour and input salary. Some formatting. Added options html. Added must have and input to start timer.

--- money.js
+++ money.js
@@ -9,32 +9,46 @@ const startButton = document.querySelector("#startButton")
 const stopButton = document.querySelector("#stopButton")
 const resetButton = document.querySelector('#resetButton')
 const salaryInputEl = document.createElement('input')
+const timeUnitOptionElement = document.getElementById('timeUnitSelect').innerHTML
 
 //Create Variables
 let timePlaceHolder = 0 + " seconds"
-const salary = 30000 //kr per hours lets say
-timerIsOn = false
+let timerIsOn = false
 let timeIn100th
+let salary
 
 function setTime(){
     timeIn100th = 0
+    salary = 0
 }
 setTime()
 
 //Assign attributes to elements
 timeDisplay.innerText = timePlaceHolder
 moneyDisplay.innerText = salary + " coins"
-salaryInputEl.placeholder = "inputSalaryElement"
+
+salaryInputEl.placeholder = "Input your Salary"
+salaryInputEl.setAttribute("id", "userSalary")
 
 ContainerDiv.appendChild(timeDisplay)
 ContainerDiv.appendChild(moneyDisplay)
 ContainerDiv.appendChild(salaryInputEl)
 
 
-//Function for elements
+//Functions for elements
+
+//for every keypress check the salary value
+document.body.addEventListener("keyup", ()=>{
+    if (timerIsOn == false)
+    salary = salaryInputEl.value
+    console.log(salary)
+})
+
+
+
 //Start that uses interval for timer, formats seconds to also shows deciseconds and updates element
 startButton.addEventListener("click", ()=> { 
-    if (timerIsOn == false){
+    if (timerIsOn == false && salary != 0){
         timerIsOn = true
         if (timerIsOn = true){
             timerInterval = setInterval(function(){
@@ -42,11 +56,11 @@ startButton.addEventListener("click", ()=> {
                 timeIn100th = timeIn100th + 1
                 timeInSeconds = Math.floor(timeIn100th) / 10
                 timeDisplay.innerText = timeInSeconds + " seconds"
-                moneyDisplay.innerText = salary / (720*3600) * timeInSeconds
+                moneyDisplay.innerText = (salary / (86400) * timeInSeconds).toFixed(3) + " coins"
             },100)
         }
     }
-    else console.log("Timer is already on, stop or reset?")
+    else console.log("Timer is already on, stop or reset? Input your salary please.")
     
     
     
@@ -60,6 +74,8 @@ stopButton.addEventListener("click", function stopTimer() {
 resetButton.addEventListener('click', ()=>{
     clearInterval(timerInterval)
     timeDisplay.innerText = timePlaceHolder
+    moneyDisplay.innerText = 0 + " coins"
+
     timerIsOn = false
     setTime()
 })
